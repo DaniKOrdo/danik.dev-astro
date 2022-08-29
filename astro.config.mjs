fbe9c8f71a1fb7a21108cfcdd2a1b54c873f793f
DaniK <danikordonezgarcia@gmail.com>
2022-08-29 10:26:43 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+
+import { SITE } from "./src/config.mjs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://astro.build/config
+export default defineConfig({
+  // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+  site: SITE.domain,
+  base: "/",
+
+  output: "static",
+
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap(),
+    image(),
+  ],
+
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
+});
---
+++ src/config.mjs
@@ -0,0 +1,7 @@
+export const SITE = {
+  name: "DaniK",
+  domain: "https://danik.dev",
+  github: "https://github.com/danikordo",
+
+  postsPerPage: 6,
+};
