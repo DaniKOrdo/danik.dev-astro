8842b6189c0a57d2e16dfe5467598481e034e782
DaniK <danikordonezgarcia@gmail.com>
2023-08-15 20:19:18 +0200

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,32 @@
 import path from "path";
 import { fileURLToPath } from "url";
-
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
-
 import { SITE } from "./src/config.mjs";
+import react from "@astrojs/react";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
 
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
   site: SITE.domain,
   base: "/",
-
   output: "static",
-
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    image(),
-  ],
-
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), image(), react()],
   vite: {
     resolve: {
       alias: {
-        "~": path.resolve(__dirname, "./src"),
-      },
-    },
-  },
+        "~": path.resolve(__dirname, "./src")
+      }
+    }
+  }
 });
