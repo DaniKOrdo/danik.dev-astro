c3da75ffa7a6f5887ff3b779787f399c2343d5f9
DaniK <danikordonezgarcia@gmail.com>
2023-08-16 22:48:27 +0200

    fix font size

--- src/pages/projects/to-do-react/components/DateBar.jsx
+++ src/pages/projects/to-do-react/components/DateBar.jsx
@@ -8,13 +8,13 @@ function DateBar() {
   return (
     <header className="date-bg bg-[#8b8574] flex flex-row w-full justify-between p-5 font-[Montserrat] items-center uppercase bg-right-bottom rounded-md m-3 text-white">
       <div className="flex items-center">
-        <span className="text-7xl pr-3">{day}</span>
-        <div className="flex flex-col">
+        <span className="text-6xl md:text-7xl pr-3">{day}</span>
+        <div className="flex flex-col text-lg md:text-2xl">
           <span>{monthName}</span>
           <span className="font-extralight">{year}</span>
         </div>
       </div>
-      <span>{dayName}</span>
+      <span className="text-lg md:text-xl">{dayName}</span>
     </header>
   );
 }
